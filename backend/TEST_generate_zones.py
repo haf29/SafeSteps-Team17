66b76878dad8d02a8056700c324d9ae4d3b5f6a7
Vortexx-hash <ali.m.s.naji1234@gmail.com>
2025-08-02 21:18:52 +0300

    My local changes

---
+++ backend/TEST_generate_zones.py
@@ -0,0 +1,20 @@
+
+#from dotenv import load_dotenv
+#load_dotenv()
+from api.services.h3_utils import generate_zone_ids
+
+beirut = {
+    "type": "Polygon",
+    "coordinates": [[
+        [35.59162498298079, 33.940065201128334],
+        [35.572005518255196, 33.90490653187199],
+        [35.46987428475077, 33.90028936404184],
+        [35.480332490494646, 33.79608579493343],
+        [35.52212339964245, 33.82114173341516],
+        [35.550877998954945, 33.85122835122202],
+        [35.59162498298079, 33.940065201128334]
+    ]]
+}
+
+zones = generate_zone_ids(beirut, 9)
+print("Sample zone IDs:", zones[:5], "… total:", len(zones))
---
+++ backend/api/services/auth.py
@@ -0,0 +1,53 @@
+import os
+from typing import Tuple
+import boto3
+from botocore.exceptions import ClientError
+from fastapi import HTTPException,status 
+
+from app.models.user import UserSignUp, UserSignIn, UserToken
+
+USER_POOL_ID = os.getenv("COGNITO_USER_POOL_ID")
+CLIENT_ID = os.getenv("COGNITO_APP_CLIENT_ID")
+
+cognito = boto3.client("cognito-idp", region_name=os.getenv("AWS_REGION", "eu_north-1" ))
+
+def sign_up(user : UserSignUp) -> str: 
+    try:
+        response = cognito.sign_up(
+            ClientId=CLIENT_ID,
+            Username=user.email,
+            Password=user.password,
+            UserAttributes=[{"Name": "email", "Value": user.email}]
+        )
+        return response["UserSub"]
+    except ClientError as e:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+def confirm_sign_up(email: str, code: str) -> None:
+    cognito.confirm_sign_up(
+        ClientId=CLIENT_ID,
+        Username=email,
+        ConfirmationCode=code
+    )
+    return 
+def sign_in(credentials: UserSignIn) -> UserToken:
+    try:
+        response = cognito.admin_initiate_auth(
+            ClientId=CLIENT_ID,
+            AuthFlow="USER_PASSWORD_AUTH",
+            AuthParameters={"USERNAME": credentials.email, "PASSWORD": credentials.password},
+        )
+        auth = response["AuthenticationResult"]
+        return UserToken(
+            access_token=auth["AccessToken"],
+            refresh_token=auth["RefreshToken"],
+            id_token=auth["IdToken"],
+            expires_in=auth["ExpiresIn"]
+        )
+    except ClientError as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    
+
+
+
+
